fc5f60ed7adba59d1e69ee07009cc09c455ac8d6
Guilherme <you@example.com>
2019-04-08 22:45:16 -0300

    cliente pt1

--- client.js
+++ client.js
@@ -1,5 +1,8 @@
 const config = require('./utils/config');
+const bodyParser = require('body-parser');
 
+const express = require('express')
+config.app.use(express.json())
 config.app.get('/getStates', (req, res) => {
 
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -17,11 +20,12 @@ config.app.get('/getCitys', (req, res) => {
 
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested");
-  console.log(req.params)
-  // let sql = {text: 'select * from c'};
+  console.log(req.query);
+  // let id = 
+  let sql = {text: `select * from city where id_estate= ${req.query.id_estate}` };
   
-  // db.query(sql).then(result => {
-  //   res.setHeader('Content-Type', 'application/json')
-  //   res.end(JSON.stringify(result.rows))
-  // }).catch(e => console.error(e))
+  db.query(sql).then(result => {
+    res.setHeader('Content-Type', 'application/json')
+    res.end(JSON.stringify(result.rows))
+  }).catch(e => console.error(e))
 });
